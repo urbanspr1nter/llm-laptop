1bf9a32ef23c2fbf6178da7cf9478a8fd224cf26
Roger Ngo <urbanspr1nter@gmail.com>
2026-05-23 10:11:05 -0700

    Qwen3.5-9B

---
+++ gguf-models/download-qwen3.5-9b-ud-q4_k_xl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p "$HOME/models/Qwen3.5-9B-GGUF"
+wget -O "$HOME/models/Qwen3.5-9B-GGUF/Qwen3.5-9B-UD-Q4_K_XL.gguf" \
+    https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/Qwen3.5-9B-UD-Q4_K_XL.gguf
+wget -O "$HOME/models/Qwen3.5-9B-GGUF/mmproj-BF16.gguf" \
+    https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/mmproj-BF16.gguf
+
+touch "$HOME/models/run-qwen3.5-9b-GGUF.sh"
+chmod +x "$HOME/models/run-qwen3.5-9b-GGUF.sh"
+    cat >> "$HOME/models/run-qwen3.5-9b-GGUF.sh" <<'EOF'
+#!/bin/bash
+
+"$HOME/bin/llama.cpp/bin/llama-server" \
+    -m "$HOME/models/Qwen3.5-9B-GGUF/Qwen3.5-9B-UD-Q4_K_XL.gguf"\
+    --mmproj "$HOME/models/Qwen3.5-9B-GGUF/mmproj-BF16.gguf" \
+    -a qwen3.5-9b \
+    -fa on \
+    -c 131072 \
+    --host 0.0.0.0 \
+    --port 8000 \
+    -np 1
+EOF
