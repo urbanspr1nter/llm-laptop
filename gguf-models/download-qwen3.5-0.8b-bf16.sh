50cde0995d400f36857379b732907dcf14e5cf49
Roger Ngo <urbanspr1nter@gmail.com>
2026-06-07 14:43:05 -0700

    Testing Qwen3.5 0.8B

--- gguf-models/download-qwen3.5-0.8b-bf16.sh
+++ gguf-models/download-qwen3.5-0.8b-bf16.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p "$HOME/models/Qwen3.5-0.8-GGUF"
-wget -O "$HOME/models/Qwen3.5-0.8-GGUF/Qwen3.5-0.8B-BF16.gguf" \
+wget -O "$HOME/models/Qwen3.5-0.8B-GGUF/Qwen3.5-0.8B-BF16.gguf" \
     https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/Qwen3.5-0.8B-BF16.gguf
-wget -O "$HOME/models/Qwen3.5-0.8-GGUF/mmproj-BF16.gguf" \
+wget -O "$HOME/models/Qwen3.5-0.8B-GGUF/mmproj-BF16.gguf" \
     https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/mmproj-BF16.gguf
 
 touch "$HOME/models/run-qwen3.5-0.8b-GGUF.sh"
